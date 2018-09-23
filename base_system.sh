bdd688a607686cf023c2e45b1a0e2d9e69de210a
Leonardo Costa <leonardocosta@ua.pt>
2018-09-23 01:23:24 +0100

    deletados ficheiros relacionados com bash provisioning

--- base_system.sh
+++
@@ -1,9 +0,0 @@
-export DEBIAN_FRONTEND=noninteractive
-
-#Update System and install apt-get frontend Aptitude
-apt-get -qy update
-cat /vagrant/packages.txt | egrep -v "^\s*(#|$)" | xargs apt-get -qy install
-
-#Setup etckeeper
-cp /vagrant/etckeeper.conf /etc/etckeeper/etckeeper.conf
-
